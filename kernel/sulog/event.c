088ed358cc9e18ddf6b18c83157568d9bed54488
oyzc0227-boop <oyzc0227@outlook.com>
2026-05-24 19:02:00 +0800

    更新 event.c

--- kernel/sulog/event.c
+++ kernel/sulog/event.c
@@ -368,7 +368,7 @@ static struct ksu_sulog_pending_event *ksu_sulog_capture_grant_root(const struct
     struct ksu_sulog_event *event;
 
     // This is actually stupid fix
-#define USER_ARG_NULL ((struct user_arg_ptr){ 0 })
+#define USER_ARG_NULL (&(struct user_arg_ptr){ 0 })
 
     pending = ksu_sulog_capture(KSU_SULOG_EVENT_IOCTL_GRANT_ROOT, NULL, USER_ARG_NULL, gfp);
     if (!pending)
